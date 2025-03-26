224b4b3f94d0321e274ce39476e81005ff68f8b6
Alexandre3000z <joaoalexandrems3000@gmail.com>
2025-03-26 17:39:43 -0300

    Correção nome da consulta Banco de dados Sybase

--- get_empresas/views/contratos.py
+++ get_empresas/views/contratos.py
@@ -16,49 +16,49 @@ class get_contratos(APIView):
         if request.method != 'GET':
             return JsonResponse({"error": "Método não permitido, use GET"}, status=405)
         
-        query = 'SELECT * FROM bethadba.hncontrato'
+        query = 'SELECT * FROM bethadba.HRCONTRATO'
         result = fetch_data(query)
-        key_mapping = {
-        "i_contrato": "id_contrato",
-        "codi_emp": "codigo_empresa",
-        "i_tipocontrato": "id_tipocontrato",
-        "i_evento": "id_evento",
-        "data_ctr": "data_assinatura",
-        "datainicio_ctr": "datainicio_ctr",
-        "iniciofat_ctr": "datainicio_faturamento",
-        "venctofat_ctr": "mes_vencimento",
-        "diavcto_ctr": "dia_faturamento",
-        "deftermino_ctr": "definir_datafinal_ctr",
-        "datatermino_ctr": "datafinal_ctr",
-        "valor_ctr": "valor_ctr",
-        "vloriginal_ctr": "valor_original_ctr",
-        "nroempr_ctr": "num_empregados",
-        "ultrapassarnroempr_ctr": "ultrapassa_num_empregados",
-        "ativosmes_ctr": "ativos_mes",
-        "demitidosmes_ctr": "demitidos_mes",
-        "contribuintes_ctr": "contribuintes",
-        "nrolanctos_ctr": "num_lancamentos_contabeis",
-        "ultrapassarnrolanctos_ctr": "ultrapassa_num_lancamentos_contabeis",
-        "manualmentectb_ctr": "num_lancamentos_manuais_ctb",
-        "outromodulosctb_ctr": "lancamentos_outros_modulos",
-        "importadosctb_ctr": "lancamentos_via_txt_ctb",
-        "nronotas_ctr": "num_notasfiscais",
-        "ultrapassarnronotas_ctr": "ultrapassa_num_notasfiscais",
-        "manualmenteefi_ctr": "lancamentos_manualmente_fiscal",
-        "importadosefi_ctr": "lancamentos_via_txt_fiscal",
-        "nrohoras_ctr": "num_horas_trabalhadas",
-        "ultrapassarnrohoras_ctr": "ultrapassa_num_horas_trabalhadas",
-        "escrita_ctr": "escrita_fiscal",
-        "contabilidade_ctr": "contabilidade",
-        "folha_ctr": "folha",
-        "patrimonio_ctr": "patrimonio",
-        "atualizar_ctr": "atualizar",
-        "lalur_ctr": "lalur",
-        "registro_ctr": "registro",
-        "data_nota_ctr": "data_nota_ctr",
-        "data_contab_ctr": "data_contab_ctr"
-        }
+        # key_mapping = {
+        # "i_contrato": "id_contrato",
+        # "codi_emp": "codigo_empresa",
+        # "i_tipocontrato": "id_tipocontrato",
+        # "i_evento": "id_evento",
+        # "data_ctr": "data_assinatura",
+        # "datainicio_ctr": "datainicio_ctr",
+        # "iniciofat_ctr": "datainicio_faturamento",
+        # "venctofat_ctr": "mes_vencimento",
+        # "diavcto_ctr": "dia_faturamento",
+        # "deftermino_ctr": "definir_datafinal_ctr",
+        # "datatermino_ctr": "datafinal_ctr",
+        # "valor_ctr": "valor_ctr",
+        # "vloriginal_ctr": "valor_original_ctr",
+        # "nroempr_ctr": "num_empregados",
+        # "ultrapassarnroempr_ctr": "ultrapassa_num_empregados",
+        # "ativosmes_ctr": "ativos_mes",
+        # "demitidosmes_ctr": "demitidos_mes",
+        # "contribuintes_ctr": "contribuintes",
+        # "nrolanctos_ctr": "num_lancamentos_contabeis",
+        # "ultrapassarnrolanctos_ctr": "ultrapassa_num_lancamentos_contabeis",
+        # "manualmentectb_ctr": "num_lancamentos_manuais_ctb",
+        # "outromodulosctb_ctr": "lancamentos_outros_modulos",
+        # "importadosctb_ctr": "lancamentos_via_txt_ctb",
+        # "nronotas_ctr": "num_notasfiscais",
+        # "ultrapassarnronotas_ctr": "ultrapassa_num_notasfiscais",
+        # "manualmenteefi_ctr": "lancamentos_manualmente_fiscal",
+        # "importadosefi_ctr": "lancamentos_via_txt_fiscal",
+        # "nrohoras_ctr": "num_horas_trabalhadas",
+        # "ultrapassarnrohoras_ctr": "ultrapassa_num_horas_trabalhadas",
+        # "escrita_ctr": "escrita_fiscal",
+        # "contabilidade_ctr": "contabilidade",
+        # "folha_ctr": "folha",
+        # "patrimonio_ctr": "patrimonio",
+        # "atualizar_ctr": "atualizar",
+        # "lalur_ctr": "lalur",
+        # "registro_ctr": "registro",
+        # "data_nota_ctr": "data_nota_ctr",
+        # "data_contab_ctr": "data_contab_ctr"
+        # }
         
-        filtered_result = rename_key(result, key_mapping)
+        # filtered_result = rename_key(result, key_mapping)
         
-        return JsonResponse({"Contratos": filtered_result}, safe=False)
+        return JsonResponse({"Contratos": result}, safe=False)
