3e0b351228894fba03150584897787cec6cbf8d0
Alexandre3000z <joaoalexandrems3000@gmail.com>
2025-03-26 14:16:49 -0300

    Aplicando correções na autenticação do tolen

--- setup/authentication.py
+++ setup/authentication.py
@@ -4,16 +4,20 @@ from rest_framework.authtoken.models import Token
 
 class BodyTokenAuthentication(BaseAuthentication):
     def authenticate(self, request):
+        # Tenta obter o token primeiro do corpo (se for JSON) ou dos parâmetros da query
         token_key = (
             request.data.get('api_token') if hasattr(request, 'data') else None
         ) or request.query_params.get('api_token')
 
+        # Caso o token não seja encontrado
         if not token_key:
-            return None  # sem token, DRF tenta a próxima auth class (ou nega)
+            return None  # sem token, DRF tenta a próxima classe de autenticação (ou nega)
 
+        # Verifica se o token é válido no banco de dados
         try:
             token = Token.objects.get(key=token_key)
         except Token.DoesNotExist:
             raise AuthenticationFailed('Token inválido.')
 
+        # Retorna o usuário e o token se autenticado
         return (token.user, token)
