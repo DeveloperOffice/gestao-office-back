e9ea4041c6d8f6af827fd4abb2df6270de255ae1
Alexandre3000z <joaoalexandrems3000@gmail.com>
2025-03-26 17:35:09 -0300

    Correção nome das classes

--- get_empresas/urls.py
+++ get_empresas/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from get_empresas.views.listar import get_empresas
 from get_empresas.views.clientes import get_clientes
+from get_empresas.views.contratos import get_contratos
 urlpatterns = [
     path('listar', get_empresas.as_view(), name='listar_empresas'),
     path('clientes', get_clientes.as_view(), name='listar_clientes'),
-    path('contratos', get_clientes.as_view(), name='listar_contratos'),
+    path('contratos', get_contratos.as_view(), name='listar_contratos'),
 ]
