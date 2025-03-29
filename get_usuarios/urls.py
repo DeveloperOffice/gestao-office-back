d4a2af2240fa87eb09e3f3fdea7b72d12dc977a0
Alexandre3000z <joaoalexandrems3000@gmail.com>
2025-03-29 11:46:56 -0300

    Feito a requisição para as atividades de todos os usuarios da dominio por empresas

--- get_usuarios/urls.py
+++ get_usuarios/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from get_usuarios.views.lista import get_users
-from get_usuarios.views.atividades import get_atividades
+from get_usuarios.views.atividades import get_atividades, get_atividades_cliente
 
 
 urlpatterns = [
     path('listar', get_users.as_view(), name='listar_usuarios'),
     path('atividades', get_atividades.as_view(), name='atividades_usuarios'),
+    path('atividades/clientes', get_atividades_cliente.as_view(), name='atividades_usuarios_cliente'),
 ]
