a376c67ae03c835c82cade6520379e260b4bf8aa
Gabriel <gabrielcardososilva290@gmail.com>
2025-04-04 14:55:53 -0300

    justando duas querys e orgaizaçao de dados do banco feita!

--- get_empresas/services/get_faturamente_empresa.py
+++ get_empresas/services/get_faturamente_empresa.py
@@ -1,9 +1,21 @@
 from django.http import JsonResponse
 from odbc_reader.services import fetch_data
 
-def get_faturamento():
+def get_faturamento(data_inicial, data_final):
     try:
-        query = 'SELECT * FROM bethadba.HRCLIENTE'
+        query = f"""
+            SELECT
+                efsaidas.codi_emp,
+                efsaidas.dsai_sai,
+                efsaidas.vcon_sai,
+                efsaidas.vexc_sai
+                efservicos.vcon_ser
+            FROM bethadba.efsaidas,
+            bethadba.efservicos
+            WHERE efsaidas.codi_emp = efservicos.codi_emp
+                AND efsaidas.dsai_sai BETWEEN '{data_inicial}' AND '{data_final}'
+        """
+        
         result = fetch_data(query)
         if not result:
             return JsonResponse({"message": "Nenhum dado encontrado"},status=404)
