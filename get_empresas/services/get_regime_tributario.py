23c7c577e0de3705d47529991a28b351bb410d4f
Alexandre3000z <joaoalexandrems3000@gmail.com>
2025-04-04 18:55:52 -0300

    Atualização de puxar o regime tributario concluida, agora puxando Domésticas,  MEI, Produtor Rural, Micro Empresas e etc...

--- get_empresas/services/get_regime_tributario.py
+++ get_empresas/services/get_regime_tributario.py
@@ -6,9 +6,30 @@ def get_regime():
         query = """SELECT
                 EFPARAMETRO_VIGENCIA.CODI_EMP,
                 EFPARAMETRO_VIGENCIA.VIGENCIA_PAR,
-                EFPARAMETRO_VIGENCIA.RFED_PAR
+                EFPARAMETRO_VIGENCIA.RFED_PAR,
+                SIMPLESN_OPTANTE_PAR
 
                 FROM bethadba.EFPARAMETRO_VIGENCIA"""
+                
+        result = fetch_data(query)
+
+  
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500, safe=False)
+    
+    return result
+
+def get_regime_secundario(lista_id):
+    try:        
+        query = f"""SELECT
+                foparmto.codi_emp,
+                foparmto.CLASSIFICACAO_TRIBUTARIA,
+                foparmto.COOPERATIVA,
+                foparmto.CONSTRUTORA,
+                foparmto.PRODUTOR_RURAL,
+                foparmto.GERAR_ESOCIAL_DOMESTICO
+                FROM bethadba.foparmto
+                WHERE foparmto.codi_emp IN ({','.join(map(str, lista_id))}) """
                 
         result = fetch_data(query)
 
--- get_empresas/services/integrate_client_data.py
+++ get_empresas/services/integrate_client_data.py
@@ -1,30 +1,105 @@
 from get_empresas.services.get_client_data import *
+from get_empresas.services.get_regime_tributario import get_regime, get_regime_secundario
+from datetime import datetime
 import json
 
 def integrate_data():
     empresas = json.loads(get_empresa().content)
     clientes = json.loads(get_cliente().content)
- 
+    regimes = get_regime()
+    
+    #Lista que guardará todas as empresas válidas
     empresas_validas = []
 
-    # Agrupar os escritórios por código da empresa
+    #Lista que guardará todas as empresas que não se enquadram em um regime
+    empresas_sem_regime = []
+    
+    # Passo 1: Pré-processar regimes e manter só o mais recente por CODI_EMP
+    regimes_mais_recentes = {}
+    regimes_mapping = {
+                        1:'Lucro Real',
+                        2:'Micro Empresa',
+                        3:'Estimativa',
+                        4:'Empresa Pequeno Porte',
+                        5:'Lucro Presumido',
+                        6:'Regime Especial de Tributação',
+                        7:'Lucro Arbitrado',
+                        8:'Imune do IRPJ',
+                        9:'Isenta de IRPJ'
+
+                        }
+    
+    for regime in regimes:
+        codi_emp = regime['CODI_EMP']
+        vigencia = regime['VIGENCIA_PAR']  # já é datetime.date ou datetime.datetime
+
+        if codi_emp not in regimes_mais_recentes:
+            regimes_mais_recentes[codi_emp] = regime
+        else:
+            vigencia_atual = regimes_mais_recentes[codi_emp]['VIGENCIA_PAR']
+            if vigencia > vigencia_atual:
+                regimes_mais_recentes[codi_emp] = regime
+
+    # Passo 2: Integrar dados
     for empresa in empresas["Empresas"]:
         codigo = empresa.get("codigo_empresa")
         escritorios = []
 
-        # Procurar todos os escritórios para essa empresa
         for escritorio in clientes["Clientes"]:
             if escritorio.get("I_CLIENTE_FIXO") == codigo:
-                # Adicionar tanto o código do escritório quanto o I_CLIENTE
                 escritorios.append({
                     "codigo_escritorio": escritorio.get("CODI_EMP"),
                     "id_cliente_contrato": escritorio.get("I_CLIENTE")
                 })
 
-        # Se encontrou escritórios, adicionar à empresa
         if escritorios:
             empresa["escritorios"] = escritorios
-                 
-            empresas_validas.append(empresa)
+            
+        # Regime tributário mais recente
+        if codigo in regimes_mais_recentes:
+            if regimes_mais_recentes[codigo]["SIMPLESN_OPTANTE_PAR"] == "S":
+                empresa["regime_tributario"] = "Simples Nacional"
+                
+            else:    
+                regime_formatado = regimes_mapping[regimes_mais_recentes[codigo]["RFED_PAR"]]
+                empresa["regime_tributario"] = regime_formatado
+                
+                if regime_formatado in ['Estimativa', 
+                                        'Empresa Pequeno Porte', 
+                                        'Micro Empresa', 
+                                        'Regime Especial de Tributação', 
+                                        'Lucro Arbitrado']:
+                    empresas_sem_regime.append(codigo)
+            
+            
+        else:
+            empresa["regime_tributario"] = 'N/D'
+            empresas_sem_regime.append(codigo)
+    
+            
+        empresas_validas.append(empresa)
+    
+    empresas_por_codigo = {item['codigo_empresa']: item for item in empresas_validas}
+    lista_regimes_segundarios = get_regime_secundario(empresas_sem_regime)
+    
+    for i in lista_regimes_segundarios:
+        # Alterar diretamente a empresa do código atual.
+        if i["codi_emp"] in empresas_por_codigo:
+            
+            #Doméstica
+            if i["GERAR_ESOCIAL_DOMESTICO"] == 1 or i["CLASSIFICACAO_TRIBUTARIA"] == 12:
+                empresas_por_codigo[i["codi_emp"]]['regime_tributario'] = 'Doméstica'
+            
+            #Produtor Rural    
+            elif i["PRODUTOR_RURAL"] == 1 or i["CLASSIFICACAO_TRIBUTARIA"] == 6:
+                empresas_por_codigo[i["codi_emp"]]['regime_tributario'] = 'Produtor Rural'
+            
+            #MEI    
+            elif i["CLASSIFICACAO_TRIBUTARIA"] == 4:
+                empresas_por_codigo[i["codi_emp"]]['regime_tributario'] = 'MEI'
+                
+
+       
+    
     
     return {"Empresas": empresas_validas}
