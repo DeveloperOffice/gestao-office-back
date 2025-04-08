e51428636ec8241e508d73114519d52027c49e03
Alexandre3000z <joaoalexandrems3000@gmail.com>
2025-04-08 14:31:30 -0300

    Aplicando alteração que permite vizualizar a quantidade de importações por usuário

--- get_usuarios/services/get_importacoes_usuario.py
+++ get_usuarios/services/get_importacoes_usuario.py
@@ -4,9 +4,13 @@ import json
 from datetime import datetime
 
 
-def get_importacoes():
+def get_importacoes(start_date, end_date):
     try:
-        query = "SELECT * FROM bethadba.fopppatividades"
+        query = f"""SELECT codi_usu, COUNT(*) AS total_ocorrencias
+                    FROM bethadba.efsaidas
+                    WHERE dsai_sai BETWEEN '{start_date}' AND '{end_date}'
+                    GROUP BY codi_usu
+                    ORDER BY total_ocorrencias DESC"""
         result = fetch_data(query)
         
     except Exception as e:
--- get_usuarios/views/importacoes.py
+++ get_usuarios/views/importacoes.py
@@ -12,8 +12,20 @@ class get_importacao(APIView):
         if request.method != 'POST':
             return JsonResponse({"error": "Método não permitido, use POST"}, status=405)
         
-       
+        start_date = request.data.get('start_date')
+        end_date = request.data.get('end_date')
+        
+        # Verificação se foram enviadas
+        if not start_date or not end_date:
+            return JsonResponse({"error": "start_date e end_date são obrigatórios no padrão YYYY-MM-DD"}, status=400)
+
+        # Verificação de formato
+        try:
+            datetime.strptime(start_date, "%Y-%m-%d")
+            datetime.strptime(end_date, "%Y-%m-%d")
+        except ValueError:
+            return JsonResponse({"error": "As datas devem estar no formato YYYY-MM-DD"}, status=400)
     
-        result = json.loads(get_importacoes().content)
+        result = json.loads(get_importacoes(start_date, end_date).content)
         return JsonResponse({'Importacoes': result}, safe=False)
     
