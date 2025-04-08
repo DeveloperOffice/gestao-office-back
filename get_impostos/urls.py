9e6cf88e34149ccd97385bdd30b75bdfae88900d
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-04-08 15:43:10 -0300

    Removendo app inutilizado

--- get_impostos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from get_impostos.views.listar import get_impostos
-
-urlpatterns = [
-    path('', get_impostos.as_view(), name='listar_impostos'),
-
-]
