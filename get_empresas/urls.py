a16d3efbadd80087da45089118cf4cce668cecdf
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-04-08 18:35:05 -0300

    Feature/ Faturamento Empresa , permite que consigamos requisitar o faturamento da empresa baseado na soma das notas de saida e notas de serviços prestados autorizados

--- get_empresas/urls.py
+++ get_empresas/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from get_empresas.views.listar import get_empresas
-
+from get_empresas.views.faturamento import get_faturamentos
 urlpatterns = [
     path('listar', get_empresas.as_view(), name='listar_empresas'),
- 
+    path('faturamento', get_faturamentos.as_view(), name='faturamento')
 ]
