8f53b6d22c57df4a384535a7e8ea4832972613c1
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-04-08 15:34:01 -0300

    Corrigindo regime ainda

--- get_empresas/urls.py
+++ get_empresas/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from get_empresas.views.listar import get_empresas
-from get_empresas.views.contratos import get_contratos
-from get_empresas.views.regime import get_regime_empresas
+
 urlpatterns = [
     path('listar', get_empresas.as_view(), name='listar_empresas'),
-    path('contratos', get_contratos.as_view(), name='listar_contratos'),
-    path('regime', get_regime_empresas.as_view(), name='listar_Regime'),
+ 
 ]
