ebfe716253ae84bf335f0aed1cc015270041f778
Gabriel <gabrielcardososilva290@gmail.com>
2025-04-09 12:28:50 -0300

    objeto organizado e feito

--- get_empresas/services/get_contratos_data.py
+++ get_empresas/services/get_contratos_data.py
@@ -4,9 +4,17 @@ from odbc_reader.services import fetch_data
 
 def get_contratos():
     try:
-        query = 'SELECT * FROM bethadba.HRCONTRATO'
+        query = '''
+            SELECT 
+                codi_emp,
+                i_cliente,
+                DATA_INICIO,
+                DATA_TERMINO,
+                VALOR_CONTRATO
+            FROM bethadba.HRCONTRATO
+        '''
         result = fetch_data(query)
-        return {"Clientes": result}
+        return {"Contratos": result}
         
     except Exception as e:
         return {"error": str(e)}
