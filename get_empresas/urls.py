093c1806e5f6ae8fef84976c11add41b6f9dbb88
Gabriel <gabrielcardososilva290@gmail.com>
2025-04-09 12:01:16 -0300

    service e view de contrato feita com a url configurada

--- get_empresas/urls.py
+++ get_empresas/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from get_empresas.views.listar import get_empresas
 from get_empresas.views.faturamento import get_faturamentos
+from get_empresas.views.contratos import get_contrato
+
 urlpatterns = [
     path('listar', get_empresas.as_view(), name='listar_empresas'),
-    path('faturamento', get_faturamentos.as_view(), name='faturamento')
+    path('faturamento', get_faturamentos.as_view(), name='faturamento'),
+    path('contrato', get_contrato.as_view(), name='contratos')
 ]
---
+++ get_empresas/views/contratos.py
@@ -0,0 +1,18 @@
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from get_empresas.services.get_contratos_data import get_contratos
+
+class get_contrato(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        if request.method != 'POST':
+            return JsonResponse({"error": "Método não permitido, use POST"}, status=405)
+        
+        result = get_contratos()
+        
+        if "error" in result:
+            return JsonResponse(result, status=500)
+            
+        return JsonResponse(result, safe=False)
