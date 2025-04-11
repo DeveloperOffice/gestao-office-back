2298cac1f03932aa144cb5140e5717c1aef326e0
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-04-11 11:40:53 -0300

    Concluido a logica da captura de todos os funcionarios ativos por mês

--- get_main_pages/services/get_analise_cliente.py
+++ get_main_pages/services/get_analise_cliente.py
@@ -2,15 +2,17 @@ from django.http import JsonResponse
 from get_main_pages.services.get_faturamento import get_faturamento
 from get_main_pages.services.get_dados_empresa import get_empresa
 from get_main_pages.services.get_lanc_e_notas import get_importacoes_empresa
+from get_main_pages.services.get_quant_empregados import get_contratados_por_mes
+
 import logging
 logger = logging.getLogger(__name__)
 
 
 def get_dados_analise_cliente(data_inicial, data_final):
     try:
-        result = get_importacoes_empresa(data_inicial, data_final)
+        return get_contratados_por_mes(data_inicial, data_final)
 
-        return result
+        
 
     except Exception as e:
         logger.exception("Erro ao processar faturamento com filtro de meses.")
