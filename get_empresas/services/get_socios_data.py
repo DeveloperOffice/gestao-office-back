e1628ea8031702efda0e2e5a8f9b34b239d93d48
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-04-15 15:27:16 -0300

    Criado função para relacionar todos os sócios e empresas

--- get_empresas/services/get_socios_data.py
+++ get_empresas/services/get_socios_data.py
@@ -1,9 +1,9 @@
-from django.http import JsonResponse
 from odbc_reader.services import fetch_data
-from datetime import datetime
+from get_empresas.services.get_client_data import get_nome_empresa
+from collections import defaultdict
 
 
-def get_socios():
+def get_socio(codigo_empresa):
     try:
 
         query = f"""
@@ -12,11 +12,52 @@ def get_socios():
                 FOCONTRIBUINTES.I_CONTRIBUINTES,
                 FOCONTRIBUINTES.NOME,
                 FOCONTRIBUINTES.TIPO
-
                 FROM bethadba.FOCONTRIBUINTES
+                WHERE FOCONTRIBUINTES.TIPO = 'E' AND FOCONTRIBUINTES.DESLIGADO = 'N'
                 """
         result = fetch_data(query)
-        return {"Contratos": result}
+
+        # Pegando o nome de todas as empresas junto com o código para vincular aos socios
+        nomeEmpresas = get_nome_empresa()
+
+        # Inicializando um dicionário para armazenar empresas e seus sócios
+        empresas_dict = defaultdict(list)
+
+        for item in result:
+            codi_emp = item["CODI_EMP"]
+            nome_socio = item["NOME"]
+
+            # Adiciona o nome do sócio na lista da empresa (identificada pelo codi_emp)
+            empresas_dict[codi_emp].append(nome_socio)
+
+        # Inicializando um dicionário para armazenar os sócios e as empresas em que eles estão
+        socios_dict = defaultdict(list)
+
+        # Iterando sobre os dados novamente para preencher as empresas de cada sócio
+        for item in result:
+            codi_emp = item["CODI_EMP"]
+            nome_socio = item["NOME"]
+
+            # Adiciona a empresa à lista de empresas do sócio
+            socios_dict[nome_socio].append(codi_emp)
+
+        # O dicionário socios_dict agora terá o nome do sócio como chave e uma lista de empresas como valor
+
+        # Criando o resultado a partir do codigo da empresa fornecido ligando aos dicionários
+        resultado = {}
+        resultado["codi_emp"] = codigo_empresa
+        resultado["socios"] = empresas_dict[codigo_empresa]
+        resultado["dados"] = []
+
+        # Interando sobre os sócios no dicionário da empresa
+        for i in empresas_dict[codigo_empresa]:
+            lista_empresas = socios_dict[i]
+            lista_empresas.remove(
+                codigo_empresa
+            )  # Remove a 1º empresa para não aparecer novamente.
+            resultado["dados"].append({"socio": i, "empresas": lista_empresas})
+
+        return resultado
 
     except Exception as e:
         return {"error": str(e)}
--- get_empresas/views/socios.py
+++ get_empresas/views/socios.py
@@ -1,18 +1,23 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from get_empresas.services.get_contratos_data import get_contratos
+from get_empresas.services.get_socios_data import get_socio
+
 
 class get_socios(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        if request.method != 'POST':
+        if request.method != "POST":
             return JsonResponse({"error": "Método não permitido, use POST"}, status=405)
-        
-        result = get_contratos()
-        
+
+        codigo_empresa = request.data.get("codi_emp")
+
+        if not codigo_empresa:
+            return JsonResponse({"error": "'codi_emp' é obrigatório."}, safe=False)
+        result = get_socio(codigo_empresa)
+
         if "error" in result:
             return JsonResponse(result, status=500)
-            
+
         return JsonResponse(result, safe=False)
