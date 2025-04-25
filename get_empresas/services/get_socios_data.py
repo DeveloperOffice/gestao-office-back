a97c512f307b67bb8a47a37a47c840eab31ae4fd
Gabriel <gabrielcardososilva290@gmail.com>
2025-04-25 15:55:04 -0300

    colocando o CPF no json dados feito

--- get_empresas/services/get_socios_data.py
+++ get_empresas/services/get_socios_data.py
@@ -9,7 +9,8 @@ def get_socio(codigo_empresa):
             SELECT 
                 bethadba.gequadrosocietario_socios.codi_emp,
                 bethadba.gequadrosocietario_socios.i_socio,
-                bethadba.gesocios.nome
+                bethadba.gesocios.nome,
+                bethadba.gesocios.inscricao  
             FROM bethadba.gequadrosocietario_socios
             JOIN bethadba.gesocios 
               ON bethadba.gequadrosocietario_socios.i_socio = bethadba.gesocios.i_socio
@@ -45,8 +46,8 @@ def get_socio(codigo_empresa):
 
         resultado = {
             "codi_emp": codigo_empresa,
-            "nome_emp": consultaNome(codigo_empresa)["nome"],
-            "cnpj": consultaNome(codigo_empresa)["cnpj"],
+            "nome_emp": consultaNome(codigo_empresa)["nome"] if consultaNome(codigo_empresa) else "Nome não encontrado",
+            "cnpj": consultaNome(codigo_empresa)["cnpj"] if consultaNome(codigo_empresa) else "CNPJ não encontrado",
             "socios": sorted(empresas_dict[codigo_empresa]),  # ordenado pra ficar bonito
             "dados": []
         }
@@ -65,10 +66,21 @@ def get_socio(codigo_empresa):
                         "cnpj": nome_empresa["cnpj"]
                     })
 
-            resultado["dados"].append({
-                "socio": socio,
-                "empresas": lista_nome_empresas
-            })
+            # Recuperando o CPF (inscricao) diretamente do result
+            socio_info = next((item for item in result if item["nome"] == socio), None)
+            if socio_info:  # Verifica se socio_info não é None
+                resultado["dados"].append({
+                    "socio": socio,
+                    "CPF": socio_info["inscricao"],  # Agora o CPF vem direto de result
+                    "empresas": lista_nome_empresas
+                })
+            else:
+                print(f"Erro ao encontrar CPF para o sócio {socio}")  # Log de erro
+                resultado["dados"].append({
+                    "socio": socio,
+                    "CPF": "CPF não encontrado",  # Ou outra mensagem de erro
+                    "empresas": lista_nome_empresas
+                })
 
         return resultado
 
