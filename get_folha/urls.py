24502006313e0520a22ceedaa3a446031154d43b
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-04-30 10:39:56 -0300

    Criando requisição para listagem de contadores, concluida

--- get_folha/urls.py
+++ get_folha/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from get_folha.views.empregados import get_empregado
-
+from get_folha.views.contadores import get_contador
 
 urlpatterns = [
-    #Listar
-    path('empregados', get_empregado.as_view(), name='listar_empregados'),
-
+    # Listar
+    path("empregados", get_empregado.as_view(), name="listar_empregados"),
+    path("contadores", get_contador.as_view(), name="listar_contadores"),
 ]
