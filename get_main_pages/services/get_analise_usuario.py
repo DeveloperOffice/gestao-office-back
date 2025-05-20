5dcb8366188daa8f7b7d372403f7886a1b18423b
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-05-20 08:34:21 -0300

    Adicionando totais gerais

--- get_main_pages/services/get_analise_usuario.py
+++ get_main_pages/services/get_analise_usuario.py
@@ -24,7 +24,6 @@ MESES_ABREV_TO_NUM = {
 }
 MES_NUM_TO_ABREV = {v: k for k, v in MESES_ABREV_TO_NUM.items()}
 
-
 def time_to_seconds(t):
     """Converte um objeto datetime.time ou string 'HH:MM:SS' para segundos."""
     if t is None:
@@ -35,7 +34,6 @@ def time_to_seconds(t):
         except Exception:
             return 0
     return t.hour * 3600 + t.minute * 60 + t.second
-
 
 def get_usuarios():
     try:
@@ -48,7 +46,6 @@ def get_usuarios():
     except Exception as e:
         logger.error(f"Erro ao carregar usuários: {e}")
         return {}
-
 
 def get_tempo_gasto_usuario_empresa_mes(start_date, end_date):
     """Retorna dicionário com tempo gasto (em segundos) por usuário, empresa e mês/ano."""
@@ -90,7 +87,6 @@ def get_tempo_gasto_usuario_empresa_mes(start_date, end_date):
         logger.error(f"Erro ao carregar tempo gasto: {e}")
         return {}
 
-
 def agrupar_por_usuario_empresa_mes(query_result, data_key):
     """Agrupa contagens por usuário, empresa e mês/ano."""
     dados = {}
@@ -114,7 +110,6 @@ def agrupar_por_usuario_empresa_mes(query_result, data_key):
         )
 
     return dados
-
 
 def get_analise_usuario(start_date, end_date):
     try:
@@ -158,144 +153,77 @@ def get_analise_usuario(start_date, end_date):
         """
 
         saidas = agrupar_por_usuario_empresa_mes(fetch_data(query_saida), "data_ref")
-        entradas = agrupar_por_usuario_empresa_mes(
-            fetch_data(query_entrada), "data_ref"
-        )
-        servicos = agrupar_por_usuario_empresa_mes(
-            fetch_data(query_servicos), "data_ref"
-        )
-        lancamentos = agrupar_por_usuario_empresa_mes(
-            fetch_data(query_lancamentos), "data_ref"
-        )
-        lancamentos_manuais = agrupar_por_usuario_empresa_mes(
-            fetch_data(query_lancamentos_manuais), "data_ref"
-        )
-
+        entradas = agrupar_por_usuario_empresa_mes(fetch_data(query_entrada), "data_ref")
+        servicos = agrupar_por_usuario_empresa_mes(fetch_data(query_servicos), "data_ref")
+        lancamentos = agrupar_por_usuario_empresa_mes(fetch_data(query_lancamentos), "data_ref")
+        lancamentos_manuais = agrupar_por_usuario_empresa_mes(fetch_data(query_lancamentos_manuais), "data_ref")
         tempo_gasto = get_tempo_gasto_usuario_empresa_mes(start_date, end_date)
 
         todos_usuarios = set()
-        for d in [
-            saidas,
-            entradas,
-            servicos,
-            lancamentos,
-            lancamentos_manuais,
-            tempo_gasto,
-        ]:
+        for d in [saidas, entradas, servicos, lancamentos, lancamentos_manuais, tempo_gasto]:
             todos_usuarios.update(d.keys())
 
         resultado_final = []
 
         for usuario_id in todos_usuarios:
             empresas = set()
-            for fonte in [
-                saidas,
-                entradas,
-                servicos,
-                lancamentos,
-                lancamentos_manuais,
-                tempo_gasto,
-            ]:
+            for fonte in [saidas, entradas, servicos, lancamentos, lancamentos_manuais, tempo_gasto]:
                 if usuario_id in fonte:
                     empresas.update(fonte[usuario_id].keys())
 
+            total_usuario_entradas = total_usuario_saidas = total_usuario_servicos = 0
+            total_usuario_lancamentos = total_usuario_lancamentos_manuais = total_usuario_tempo_gasto = 0
             empresas_list = []
-            total_usuario_entradas = 0
-            total_usuario_saidas = 0
-            total_usuario_servicos = 0
-            total_usuario_lancamentos = 0
-            total_usuario_lancamentos_manuais = 0
-            total_usuario_tempo_gasto = 0
 
             for codi_emp in empresas:
                 meses_todos = set()
-                for fonte in [
-                    saidas,
-                    entradas,
-                    servicos,
-                    lancamentos,
-                    lancamentos_manuais,
-                    tempo_gasto,
-                ]:
-                    meses_todos.update(
-                        fonte.get(usuario_id, {}).get(codi_emp, {}).keys()
-                    )
+                for fonte in [saidas, entradas, servicos, lancamentos, lancamentos_manuais, tempo_gasto]:
+                    meses_todos.update(fonte.get(usuario_id, {}).get(codi_emp, {}).keys())
 
-                meses_ordenados = sorted(
-                    meses_todos,
-                    key=lambda x: (
-                        int(x.split("/")[1]),
-                        MESES_ABREV_TO_NUM[x.split("/")[0]],
-                    ),
-                )
+                meses_ordenados = sorted(meses_todos, key=lambda x: (
+                    int(x.split("/")[1]), MESES_ABREV_TO_NUM[x.split("/")[0]]
+                ))
 
+                total_entradas = total_saidas = total_servicos = 0
+                total_lancamentos = total_lancamentos_manuais = total_tempo_gasto_empresa = 0
                 dados_empresa = {}
-                total_entradas = 0
-                total_saidas = 0
-                total_servicos = 0
-                total_lancamentos = 0
-                total_lancamentos_manuais = 0
-                total_tempo_gasto_empresa = 0
 
                 for mes in meses_ordenados:
-                    entradas_mes = (
-                        entradas.get(usuario_id, {}).get(codi_emp, {}).get(mes, 0)
-                    )
-                    saidas_mes = (
-                        saidas.get(usuario_id, {}).get(codi_emp, {}).get(mes, 0)
-                    )
-                    servicos_mes = (
-                        servicos.get(usuario_id, {}).get(codi_emp, {}).get(mes, 0)
-                    )
-                    lancamentos_mes = (
-                        lancamentos.get(usuario_id, {}).get(codi_emp, {}).get(mes, 0)
-                    )
-                    lancamentos_manuais_mes = (
-                        lancamentos_manuais.get(usuario_id, {})
-                        .get(codi_emp, {})
-                        .get(mes, 0)
-                    )
-                    tempo_gasto_mes = (
-                        tempo_gasto.get(usuario_id, {}).get(codi_emp, {}).get(mes, 0)
-                    )
+                    e = entradas.get(usuario_id, {}).get(codi_emp, {}).get(mes, 0)
+                    s = saidas.get(usuario_id, {}).get(codi_emp, {}).get(mes, 0)
+                    svc = servicos.get(usuario_id, {}).get(codi_emp, {}).get(mes, 0)
+                    l = lancamentos.get(usuario_id, {}).get(codi_emp, {}).get(mes, 0)
+                    lm = lancamentos_manuais.get(usuario_id, {}).get(codi_emp, {}).get(mes, 0)
+                    tg = tempo_gasto.get(usuario_id, {}).get(codi_emp, {}).get(mes, 0)
 
-                    importacoes = entradas_mes + saidas_mes + servicos_mes
-
+                    importacoes = e + s + svc
                     dados_empresa[mes] = {
-                        "tempo_gasto": tempo_gasto_mes,
+                        "tempo_gasto": tg,
                         "importacoes": importacoes,
-                        "lancamentos": lancamentos_mes,
-                        "lancamentos_manuais": lancamentos_manuais_mes,
+                        "lancamentos": l,
+                        "lancamentos_manuais": lm,
                     }
 
-                    total_entradas += entradas_mes
-                    total_saidas += saidas_mes
-                    total_servicos += servicos_mes
-                    total_lancamentos += lancamentos_mes
-                    total_lancamentos_manuais += lancamentos_manuais_mes
-                    total_tempo_gasto_empresa += tempo_gasto_mes
+                    total_entradas += e
+                    total_saidas += s
+                    total_servicos += svc
+                    total_lancamentos += l
+                    total_lancamentos_manuais += lm
+                    total_tempo_gasto_empresa += tg
 
-                total_empresa = (
-                    total_entradas
-                    + total_saidas
-                    + total_servicos
-                    + total_lancamentos
-                    + total_lancamentos_manuais
-                )
-
-                empresas_list.append(
-                    {
-                        "codi_emp": codi_emp,
-                        "atividades": dados_empresa,
-                        "total_entradas": total_entradas,
-                        "total_saidas": total_saidas,
-                        "total_servicos": total_servicos,
-                        "total_lancamentos": total_lancamentos,
-                        "total_lancamentos_manuais": total_lancamentos_manuais,
-                        "total_tempo_gasto": total_tempo_gasto_empresa,
-                        "total_geral": total_empresa,
-                    }
-                )
+                total_empresa = total_entradas + total_saidas + total_servicos + total_lancamentos + total_lancamentos_manuais
+                empresas_list.append({
+                    "codi_emp": codi_emp,
+                    "atividades": dados_empresa,
+                    "total_entradas": total_entradas,
+                    "total_saidas": total_saidas,
+                    "total_servicos": total_servicos,
+                    "total_lancamentos": total_lancamentos,
+                    "total_lancamentos_manuais": total_lancamentos_manuais,
+                    "total_tempo_gasto": total_tempo_gasto_empresa,
+                    "total_importacoes": total_entradas + total_saidas + total_servicos,
+                    "total_geral": total_empresa,
+                })
 
                 total_usuario_entradas += total_entradas
                 total_usuario_saidas += total_saidas
@@ -304,30 +232,34 @@ def get_analise_usuario(start_date, end_date):
                 total_usuario_lancamentos_manuais += total_lancamentos_manuais
                 total_usuario_tempo_gasto += total_tempo_gasto_empresa
 
-            total_geral_usuario = (
-                total_usuario_entradas
-                + total_usuario_saidas
-                + total_usuario_servicos
-                + total_usuario_lancamentos
-                + total_usuario_lancamentos_manuais
-            )
+            total_geral_usuario = total_usuario_entradas + total_usuario_saidas + total_usuario_servicos + total_usuario_lancamentos + total_usuario_lancamentos_manuais
+            resultado_final.append({
+                "usuario_id": usuario_id,
+                "nome_usuario": usuarios_map.get(usuario_id, usuario_id),
+                "empresas": empresas_list,
+                "total_entradas": total_usuario_entradas,
+                "total_saidas": total_usuario_saidas,
+                "total_servicos": total_usuario_servicos,
+                "total_lancamentos": total_usuario_lancamentos,
+                "total_lancamentos_manuais": total_usuario_lancamentos_manuais,
+                "total_tempo_gasto": total_usuario_tempo_gasto,
+                "total_importacoes": total_usuario_entradas + total_usuario_saidas + total_usuario_servicos,
+                "total_geral": total_geral_usuario,
+            })
 
-            resultado_final.append(
-                {
-                    "usuario_id": usuario_id,
-                    "nome_usuario": usuarios_map.get(usuario_id, f"{usuario_id}"),
-                    "empresas": empresas_list,
-                    "total_entradas": total_usuario_entradas,
-                    "total_saidas": total_usuario_saidas,
-                    "total_servicos": total_usuario_servicos,
-                    "total_lancamentos": total_usuario_lancamentos,
-                    "total_lancamentos_manuais": total_usuario_lancamentos_manuais,
-                    "total_tempo_gasto": total_usuario_tempo_gasto,
-                    "total_geral": total_geral_usuario,
-                }
-            )
+        # Cálculo dos totais gerais
+        totais_gerais = {
+            "total_entradas": sum(u["total_entradas"] for u in resultado_final),
+            "total_saidas": sum(u["total_saidas"] for u in resultado_final),
+            "total_servicos": sum(u["total_servicos"] for u in resultado_final),
+            "total_lancamentos": sum(u["total_lancamentos"] for u in resultado_final),
+            "total_lancamentos_manuais": sum(u["total_lancamentos_manuais"] for u in resultado_final),
+            "total_tempo_gasto": sum(u["total_tempo_gasto"] for u in resultado_final),
+            "total_importacoes": sum(u["total_importacoes"] for u in resultado_final),
+            "total_geral": sum(u["total_geral"] for u in resultado_final),
+        }
 
-        return JsonResponse(resultado_final, safe=False)
+        return JsonResponse({"analises": resultado_final, "totais_gerais": totais_gerais}, safe=False)
 
     except Exception as e:
         tb = traceback.format_exc()
@@ -350,7 +282,6 @@ def get_analise_por_sistema(start_date, end_date):
         registros = fetch_data(query)
 
         resultado = {}
-
         for registro in registros:
             sistema = registro["sistema"]
             usuario = registro["usuario"]
@@ -360,51 +291,33 @@ def get_analise_por_sistema(start_date, end_date):
 
             if isinstance(data_log, str):
                 data_log = datetime.strptime(data_log, "%Y-%m-%d")
-
             inicio_segundos = time_to_seconds(inicio_log)
             fim_segundos = time_to_seconds(fim_log)
-
             duracao = fim_segundos - inicio_segundos
             if duracao < 0:
                 duracao += 24 * 3600
-
             mes_ano = f"{MES_NUM_TO_ABREV[data_log.month]}/{data_log.year}"
 
-            resultado.setdefault(sistema, {})
-            resultado[sistema].setdefault(usuario, {})
-            resultado[sistema][usuario][mes_ano] = (
-                resultado[sistema][usuario].get(mes_ano, 0) + duracao
-            )
+            resultado.setdefault(sistema, {}).setdefault(usuario, {})
+            resultado[sistema][usuario][mes_ano] = resultado[sistema][usuario].get(mes_ano, 0) + duracao
 
         resposta = {}
         for sistema, usuarios in resultado.items():
             total_sistema = 0
             usuarios_list = []
-
             for usuario, meses in usuarios.items():
-                meses_ordenados = sorted(
-                    meses.items(),
-                    key=lambda x: (
-                        int(x[0].split("/")[1]),
-                        MESES_ABREV_TO_NUM[x[0].split("/")[0]],
-                    ),
-                )
-                atividades_formatadas = {mes: tempo for mes, tempo in meses_ordenados}
-                total_usuario = sum(atividades_formatadas.values())
+                meses_ordenados = sorted(meses.items(), key=lambda x: (
+                    int(x[0].split("/")[1]), MESES_ABREV_TO_NUM[x[0].split("/")[0]]
+                ))
+                atividades = {mes: tempo for mes, tempo in meses_ordenados}
+                total_usuario = sum(atividades.values())
                 total_sistema += total_usuario
-
-                usuarios_list.append(
-                    {
-                        "usuario": usuario,
-                        "atividades": atividades_formatadas,
-                        "total_usuario": total_usuario,
-                    }
-                )
-
-            resposta[sistema] = {
-                "usuarios": usuarios_list,
-                "total_sistema": total_sistema,
-            }
+                usuarios_list.append({
+                    "usuario": usuario,
+                    "atividades": atividades,
+                    "total_usuario": total_usuario,
+                })
+            resposta[sistema] = {"usuarios": usuarios_list, "total_sistema": total_sistema}
 
         return JsonResponse(resposta, safe=False)
 
