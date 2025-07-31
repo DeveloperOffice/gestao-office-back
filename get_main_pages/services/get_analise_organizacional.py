bc37319f7e67b8c8c5dfc895c6815e819859ec30
Gabriel <gabrielcardososilva290@gmail.com>
2025-07-31 10:35:27 -0300

    nome das empresas e dos funcionarios colocados

--- get_main_pages/services/get_analise_organizacional.py
+++ get_main_pages/services/get_analise_organizacional.py
@@ -19,8 +19,10 @@ def get_organizacional():
     query = """
     SELECT
         g.codi_emp,
+        emp.nome_emp AS nome_empresa,
         r.demissao,
         g.i_empregados AS i_empregado,
+        f.nome AS nome_empregado,
         f.i_sindicatos,
         COALESCE(a.novo_salario, f.salario) AS salario,
         g.aviso_previo_base AS aviso_previo,
@@ -54,29 +56,32 @@ def get_organizacional():
 
         COALESCE(fl.valor_ferias, 0) AS valor_ferias
 
-    FROM bethadba.foguiagrfc g
-    LEFT JOIN bethadba.foempregados f 
-        ON g.codi_emp = f.codi_emp AND g.i_empregados = f.i_empregados
-    LEFT JOIN bethadba.forescisoes r 
-        ON f.codi_emp = r.codi_emp AND f.i_empregados = r.i_empregados
-    LEFT JOIN (
-        SELECT 
-            codi_emp, 
-            i_empregados,
-            MAX(novo_salario) AS novo_salario  
-        FROM bethadba.foaltesal
-        GROUP BY codi_emp, i_empregados
-    )  a
-        ON f.codi_emp = a.codi_emp AND f.i_empregados = a.i_empregados
-    LEFT JOIN (
-        SELECT
-            codi_emp,
-            i_empregados,   
-            SUM(COALESCE(valor_informado, valor_calculado)) AS valor_ferias
-        FROM bethadba.foferias_lancamentos
-        GROUP BY codi_emp, i_empregados
-    ) fl
-        ON f.codi_emp = fl.codi_emp AND f.i_empregados = fl.i_empregados
+        FROM bethadba.foguiagrfc g
+        LEFT JOIN bethadba.foempregados f 
+            ON g.codi_emp = f.codi_emp AND g.i_empregados = f.i_empregados
+        LEFT JOIN bethadba.forescisoes r 
+            ON f.codi_emp = r.codi_emp AND f.i_empregados = r.i_empregados
+        LEFT JOIN bethadba.geempre emp
+            ON g.codi_emp = emp.codi_emp AND emp.stat_emp = 'A'
+        LEFT JOIN (
+            SELECT 
+                codi_emp, 
+                i_empregados,
+                MAX(novo_salario) AS novo_salario  
+            FROM bethadba.foaltesal
+            GROUP BY codi_emp, i_empregados
+        )  a
+            ON f.codi_emp = a.codi_emp AND f.i_empregados = a.i_empregados
+        LEFT JOIN (
+            SELECT
+                codi_emp,
+                i_empregados,   
+                SUM(COALESCE(valor_informado, valor_calculado)) AS valor_ferias
+            FROM bethadba.foferias_lancamentos
+            GROUP BY codi_emp, i_empregados
+        ) fl
+            ON f.codi_emp = fl.codi_emp AND f.i_empregados = fl.i_empregados
+
 
     WHERE f.admissao IS NOT NULL;
     """
@@ -85,12 +90,14 @@ def get_organizacional():
         raw_data = fetch_data(query)
         agrupado = defaultdict(list)
         folha_total = defaultdict(float)
+        empresas_nomes = {}
 
         meses_nomes = {
         1: "janeiro", 2: "feveiro", 3: "março", 4: "abril", 5: "maio", 6: "junho",
         7: "julho", 8: "agosto", 9: "setembro", 10: "outubro", 11: "novembro", 12: "dezembro"
         }
 
+        
         for row in raw_data:
             salario = float(row["salario"] or 0)
             codigo_sindicato = row.get("i_sindicatos")
@@ -99,9 +106,13 @@ def get_organizacional():
             mes_base_num = sindicato_info["mes_base"]
             mes_base = meses_nomes.get(mes_base_num, "mês desconhecido")
 
+            empresas_nomes[row["codi_emp"]] = row.get("nome_empresa", "Empresa Desconhecida")
+
+
             agrupado[row["codi_emp"]].append({
                 "demissao_debug": row.get("demissao"),
                 "i_empregado": row["i_empregado"],
+                "nome_empregado": row.get("nome_empregado", "NOME DESCONHECIDO"),
                 "salario": row["salario"],
                 "aviso_previo": row["aviso_previo"],
                 "decimo_terceiro_rescisao": row["decimo_terceiro_rescisao"],
@@ -111,14 +122,16 @@ def get_organizacional():
                 "mes_base":  mes_base  
             })
 
+
             folha_total[row["codi_emp"]] += salario
 
         return {
             "dados": [
                 {
                     "codi_emp": codi_emp,
+                    "nome_empresa": empresas_nomes.get(codi_emp, "Empresa Desconhecida"),
                     "folha_mensal": round(folha_total[codi_emp], 2),
-                    "empregados": empregados
+                    "empregados": agrupado[codi_emp],
                 }
                 for codi_emp, empregados in agrupado.items()
             ]
